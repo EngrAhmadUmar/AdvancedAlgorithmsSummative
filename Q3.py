3566bead1f375bfbe8ead873f6525a1c3407574e
Ahmad Ahmed <zcaptain2002@gmail.com>
2021-12-13 21:41:22 +0200

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -3,30 +3,31 @@ key = 2
 
 # Here we will be creating our encryption function which will take in two parameters our key and text
 def encryption(text, key):
-    # Initializing the variable where we will store encrypted message
-    encrypted_message = ""
-
     # Creating our variable that will help us track our location in our 
     # String and to help us make sure we are still less than the key.
     start = 0
+    
+    # Initializing the variable where we will store encrypted message
+    result = ""
+
 
     # Here we loop through the key and while are still inbound our key, 
     # We loop through the text in steps of our key and add characters to our result variable
     while start < key:
         for j in range(start, len(text), key):
             # Formulating our result with characters gotten in steps of our key.
-            encrypted_message += text[j]
+            result += text[j]
             # Incrementing our start variable so we go to the next index.*-+-
         start += 1
     # Returning our result
-    return encrypted_message
+    return result
 encrypted = encryption(textinput, key)
 
 # Here we will be creating our decryption function which will take in two parameters our key and encrypted text
 def decryption(encrypted, key):
     # Creating the variables needed for the decryption
     start = 0
-    decrypted_message = ''
+    result = ''
 
     # Checking for edge cases when the length of text and the key is even
     if len(encrypted) % 2 == 0 and key % 2 == 0:
@@ -38,7 +39,7 @@ def decryption(encrypted, key):
             # loop through the length of the word starting at start and take partition steps every time
             for i in range(start, len(encrypted), partition):
                 # Concatenating our characters to our result variable
-                decrypted_message += encrypted[i]
+                result += encrypted[i]
             # Incrementing our start variable so we go to the next index.
             start += 1
 
@@ -51,14 +52,14 @@ def decryption(encrypted, key):
         while start < partition:
             partition += 1
             # Adding the characters to our result variable
-            decrypted_message += encrypted[start]
+            result += encrypted[start]
             # Here we beginning looping through our words starting at start
             for i in range(start, len(encrypted)):
                 # Getting our next index to add to our result
-                c = i + partition
-                decrypted_message += encrypted[c]
+                nextindex = i + partition
+                result += encrypted[nextindex]
                 # Getting the next index to add to the answer
-                decrypted_message += encrypted[c + key]
+                result += encrypted[nextindex + key]
                 # Stopping our loop
                 break
 
@@ -67,10 +68,10 @@ def decryption(encrypted, key):
             # Incrementing our start variable so we go to the next index.
             start += 1
         # Concatenating the last character of our partition to our result variable
-        decrypted_message += encrypted[partition]
+        result += encrypted[partition]
 
     # Returning decrypted_message
-    return decrypted_message
+    return result
 decrypted = decryption(encrypted, key)
 
 print("Plain text: " + textinput)
